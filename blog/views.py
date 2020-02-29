8bf2e348f15006e41a320487c467bde28afc4ddd
JasonWu <47469275+JasonWurunfei@users.noreply.github.com>
2020-02-29 20:54:06 +0800

    finished editing feature

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import BlogPost
 from .forms import BlogForm
 from django.contrib.auth.models import User
+from datetime import datetime
 
 def blogsView(request):
     """Display all blog posts"""
@@ -36,4 +37,29 @@ def newBlogView(request):
     else:
         form = BlogForm(initial=initial_data)
 
-    return render(request, 'blog/blog_edit.html', {'form': form})
+    post_url = '/blog/new/'
+    return render(request, 'blog/blog_edit.html', {'form': form, 'post_url': post_url})
+
+
+@login_required
+def editBlogView(request, pk):
+    blog = get_object_or_404(BlogPost, pk=pk)
+    initial_data = {
+        'user': blog.user,
+        'body': blog.body,
+        'title': blog.title,
+        'created_date': blog.created_date,
+        'mod_date': datetime.now(),
+    }
+    if request.method == "POST":
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:blog')
+        else:
+            print(form.errors)
+    else:
+        form = BlogForm(initial=initial_data, instance=blog)
+
+    post_url = '/blog/new/' + str(pk) + '/'
+    return render(request, 'blog/blog_edit.html', {'form': form, 'post_url': post_url})
