f6f8c1f21e671a95c4cbb1688bb97c19296c1853
JasonWu <47469275+JasonWurunfei@users.noreply.github.com>
2020-03-03 22:07:47 +0800

    finish comment feature

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Comment
 
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'pub_date', 'content_type', 'object_id')
+    list_filter = ('pub_date', 'content_type', 'object_id')
+    search_fields = ('user',)
--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+from .forms import CommentForm
 
 # Create your views here.
+
+@login_required
+def commentView(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            previous_url = request.META.get('HTTP_REFERER')
+            redirect_url = '/blog/' if previous_url==None else previous_url
+            return redirect(redirect_url)
+
